17688a07d4d731f323fea0d8a857663f5912fc49
mangoplane <devmon1@protonmail.com>
2023-10-20 12:16:53 +1100

    Use alternative ed25519 keygen from noble/ed25519

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,10 @@
-import { x25519 } from "@noble/curves/ed25519";
 import { X25519Pair } from "./types";
+import * as ed from "@noble/ed25519";
+import { sha512 } from "@noble/hashes/sha512";
+ed.etc.sha512Sync = (...m) => sha512(ed.etc.concatBytes(...m));
 
 export function generateX25519KeyPair(): X25519Pair {
-    const priv = x25519.utils.randomPrivateKey();
-    const pub = x25519.getPublicKey(priv);
+    const priv = ed.utils.randomPrivateKey();
+    const pub = ed.getPublicKey(priv);
     return { pub, priv };
 }
